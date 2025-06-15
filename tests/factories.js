96b19cb85958a0e6775831e0dc55ac24e63b707d
harsh <harsh.singh@skillup.tech>
2025-06-16 01:01:06 +0530

    feat: implement product model tests and factory with fake data generation

--- tests/factories.js
+++ tests/factories.js
@@ -1,52 +1,36 @@
 const { faker } = require('@faker-js/faker');
 const { Product, Category } = require('../src/models/product');
 
-/**
- * Factory to create fake products for testing
- */
 class ProductFactory {
-  /**
-   * Build a single product with fake data
-   * @param {Object} overrides - Properties to override
-   * @returns {Object} Product data
-   */
   static build(overrides = {}) {
     const product = {
       id: faker.datatype.number({ min: 1, max: 1000 }),
-      // ADD YOUR CODE HERE TO CREATE FAKE PRODUCTS
-      // Use faker to generate fake data for name, description, price, available, category
+      name: faker.helpers.arrayElement([
+        "Hat", "Pants", "Shirt", "Apple", "Banana", 
+        "Pots", "Towels", "Ford", "Chevy", "Hammer", "Wrench"
+      ]),
+      description: faker.lorem.paragraph(),
+      price: parseFloat(faker.commerce.price(0.5, 2000.0, 2)),
+      available: faker.datatype.boolean(),
+      category: faker.helpers.arrayElement([
+        Category.UNKNOWN, Category.CLOTHS, Category.FOOD,
+        Category.HOUSEWARES, Category.AUTOMOTIVE, Category.TOOLS
+      ])
     };
     
     return Object.assign(product, overrides);
   }
   
-  /**
-   * Build multiple products
-   * @param {number} count - Number of products to create
-   * @param {Object} overrides - Properties to override for all products
-   * @returns {Array} Array of product data
-   */
   static buildList(count, overrides = {}) {
     return Array.from({ length: count }, () => this.build(overrides));
   }
   
-  /**
-   * Create and save a product to the database
-   * @param {Object} overrides - Properties to override
-   * @returns {Promise<Product>} Created product instance
-   */
   static async create(overrides = {}) {
     const productData = this.build(overrides);
-    delete productData.id; // Let database assign ID
+    delete productData.id;
     return await Product.create(productData);
   }
   
-  /**
-   * Create and save multiple products to the database
-   * @param {number} count - Number of products to create
-   * @param {Object} overrides - Properties to override for all products
-   * @returns {Promise<Array>} Array of created product instances
-   */
   static async createList(count, overrides = {}) {
     const products = [];
     for (let i = 0; i < count; i++) {
