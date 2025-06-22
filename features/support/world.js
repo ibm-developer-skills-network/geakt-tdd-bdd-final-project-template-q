108e26e8e5c92f78801e2b9d3b54e1a927fac1d9
harsh <harsh.singh@skillup.tech>
2025-06-22 19:27:43 +0530

    feat: implement CRUD operations for products with API endpoints and tests

--- features/support/world.js
+++ features/support/world.js
@@ -1,43 +1,30 @@
-const { setWorldConstructor, BeforeAll, AfterAll, Before, After, setDefaultTimeout } = require('@cucumber/cucumber');
+const { setWorldConstructor, Before, After } = require('@cucumber/cucumber');
 const { Builder } = require('selenium-webdriver');
-const firefox = require('selenium-webdriver/firefox');
-const stoppable = require('stoppable');
-const app = require('../../src/app');
-const { closeDatabase } = require('../../src/database/connection');
-
-let server;
-const PORT = 8080;
 
 
 class CustomWorld {
   constructor({ parameters }) {
     this.parameters = parameters;
-    this.clipboard = ''; // Used for copy/paste steps
+    this.clipboard = '';
   }
-
+  
   async setDriver() {
-    const options = new firefox.Options();
-    options.addArguments('-headless');
-
-    this.driver = await new Builder()
+        this.driver = await new Builder()
       .forBrowser('firefox')
-      .setFirefoxOptions(options)
       .build();
-
+    
     await this.driver.manage().setTimeouts({
-      implicit: this.parameters.waitTimeout || 10000
+      implicit: this.parameters.waitTimeout
     });
   }
 }
 
 setWorldConstructor(CustomWorld);
 
-// Create a new browser instance for each scenario
 Before(async function () {
   await this.setDriver();
 });
 
-// Quit the browser instance after each scenario
 After(async function () {
   if (this.driver) {
     await this.driver.quit();
