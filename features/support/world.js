ca855f55a9edc65e61510c6fa8416f95d7860e91
harsh <harsh.singh@skillup.tech>
2025-06-22 18:55:48 +0530

    feat: implement CRUD operations for products with BDD tests

--- features/support/world.js
+++ features/support/world.js
@@ -1,36 +1,43 @@
-const { setWorldConstructor, Before, After } = require('@cucumber/cucumber');
+const { setWorldConstructor, BeforeAll, AfterAll, Before, After, setDefaultTimeout } = require('@cucumber/cucumber');
 const { Builder } = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
+const stoppable = require('stoppable');
+const app = require('../../src/app');
+const { closeDatabase } = require('../../src/database/connection');
+
+let server;
+const PORT = 8080;
+
 
 class CustomWorld {
   constructor({ parameters }) {
     this.parameters = parameters;
-    this.clipboard = '';
+    this.clipboard = ''; // Used for copy/paste steps
   }
-  
+
   async setDriver() {
-    const options = new chrome.Options();
-    options.addArguments('--no-sandbox');
-    options.addArguments('--headless');
-    options.addArguments('--disable-dev-shm-usage');
-    
+    const options = new firefox.Options();
+    options.addArguments('-headless');
+
     this.driver = await new Builder()
-      .forBrowser('chrome')
-      .setChromeOptions(options)
+      .forBrowser('firefox')
+      .setFirefoxOptions(options)
       .build();
-    
+
     await this.driver.manage().setTimeouts({
-      implicit: this.parameters.waitTimeout
+      implicit: this.parameters.waitTimeout || 10000
     });
   }
 }
 
 setWorldConstructor(CustomWorld);
 
+// Create a new browser instance for each scenario
 Before(async function () {
   await this.setDriver();
 });
 
+// Quit the browser instance after each scenario
 After(async function () {
   if (this.driver) {
     await this.driver.quit();
